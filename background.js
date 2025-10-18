6aa3809aadb658dc1761132045e212f39a82fe78
yunjihan1 <yunjihan33333@gmail.com>
2025-10-18 18:20:56 +0900

    점수화, 조건 추가

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("확장 프로그램 설치됨 ✅"); // 프로그램이 설치되면 설치가되었다고 메시지를 띄어주는 함수
 
-  // 우클릭 메뉴 예시
+let score = 0; // 점수 변수를 만들어 0으로 지정
+
+   // 우클릭 메뉴 예시
   chrome.contextMenus.create({ // 우클릭 메뉴를 만듬
     id: "sample", // 식별자(메뉴를 구분하기 위함)
     title: "우클릭 테스트", // 실제로 우클릭했을때 뜨는 텍스트
@@ -9,23 +11,13 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.linkUrl) {
-    // 링크 클릭 시 alert 띄우기
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: (url) => {
-        alert("클릭한 링크 URL: " + url);
-      },
-      args: [info.linkUrl]
-    });
-  } else {
-    // 페이지 클릭 시
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: () => {
-        alert("페이지를 클릭했습니다!");
+chrome.contextMenus.onClicked.addListener((info, tab) => { // 우클릭 메뉴를 등록(info: 클릭한 링크에 대한 정보, tab: 그 링크가 들어있는 탭에 대한 정보)
+if (info.linkUrl.length < 50) //url의 글자수가 50자보다 적을때
+    chrome.scripting.executeScript({ // 코드(파일)을 실행
+      target: { tabId: tab.id }, // 실행시킬 탭(프레임)을 지정
+      func: () => { // 주요 함수(main)
+        score =+ 10 //점수에 10을 더함
+        alert(score) // 점수를 알림으로 띄어줌
       }
-    });
-  }
-});
+    })
+})
